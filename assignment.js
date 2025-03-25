207cb7925e41cad8ede563024d00a5adaa81607c
baolinhdev <baolinhdev@gmail.com>
2025-03-25 19:17:57 +0700

    first commit

---
+++ assignment.js
@@ -0,0 +1,6 @@
+var fullName = "Nguyễn Văn A";
+var age = 25;
+var isStudent = true;
+console.log("H\u1ECD t\u00EAn: ".concat(fullName));
+console.log("Tu\u1ED5i: ".concat(age));
+console.log("L\u00E0 sinh vi\u00EAn: ".concat(isStudent));
---
+++ assignment.ts
@@ -0,0 +1,7 @@
+let fullName: string = "Nguyễn Văn A";
+let age: number = 18;
+let isStudent: boolean = true;
+
+console.log(`Họ tên: ${fullName}`);
+console.log(`Tuổi: ${age}`);
+console.log(`Là sinh viên: ${isStudent}`);
